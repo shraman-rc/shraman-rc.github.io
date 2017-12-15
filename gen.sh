972b382657ce87d70e6db4940e6bc9258162127f
shraman-rc <shraman@mit.edu>
2017-12-14 22:19:18 -0500

    TA stuff

--- gen.sh
+++ gen.sh
@@ -2,3 +2,7 @@
 
 echo "generating profile pic..."
 python crop.py -r 0.4 shraman.jpg
+
+echo "generating all other pics..."
+python crop.py -s 0.15 -nc ICML17.png
+python crop.py -s 0.6 -nc 6s191.png
