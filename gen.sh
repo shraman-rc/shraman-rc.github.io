9a2bea86cf3ecb4e32d0d9e1f853eee5f9406ad7
shraman-rc <shraman@mit.edu>
2017-12-15 05:32:17 -0500

    6869 project

--- gen.sh
+++ gen.sh
@@ -6,3 +6,4 @@ python crop.py -r 0.4 shraman.jpg
 echo "generating all other pics..."
 python crop.py -s 0.15 -nc ICML17.png
 python crop.py -s 0.6 -nc 6s191.png
+python crop.py -s 0.24 0.4 -nc activity_recog.png
